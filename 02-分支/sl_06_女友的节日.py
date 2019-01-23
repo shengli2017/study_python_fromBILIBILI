1ce7be0c58e39e88df31497f1440e8a0c2bac7d4
shengli <819829361@qq.com>
2019-01-24 00:08:51 +0800

    Add files via upload

---
+++ 02-分支/sl_06_女友的节日.py
@@ -0,0 +1,17 @@
+# 定义 holiday_name 字符串变量记录节目名称
+holiday_name = "生日"
+
+# 如果是 情人节 应该 买玫瑰/看电影
+if holiday_name == "情人节":
+    print("买玫瑰")
+    print("看电影")
+# 如果是 平安夜 应该 买苹果/吃大餐
+elif holiday_name == "平安夜":
+    print("买苹果")
+    print("吃大餐")
+# 如果是 生日 应该 买蛋糕
+elif holiday_name == "生日":
+    print("买蛋糕")
+# 其他的日子每天都是节日啊~
+else:
+    print("每天都是节日啊")
