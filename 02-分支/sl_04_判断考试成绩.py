714380f71676f8b6d61a7e7d144f17b009fcf5f1
shengli <819829361@qq.com>
2019-01-23 23:41:29 +0800

    Add files via upload

---
+++ 02-分支/sl_04_判断考试成绩.py
@@ -0,0 +1,10 @@
+# 练习2： 定义两个整数变量 python_score,c_score,编写代码判断成绩
+python_score = 50
+c_score = 50
+
+# 要求 只要有一门成绩 > 60 分就算合格
+if python_score >60 or c_score > 60 :
+    print("考试通过")
+else:
+    print("考试失败，请继续努力")
+
---
+++ 02-分支/sl_05_非公勿入.py
@@ -0,0 +1,5 @@
+# 练习3： 定义一个布尔型变量 is_employee, 编写代码判断是否是本公司员工
+is_employee = False
+# 如果不是提示不允许入内
+if not is_employee:
+    print("非本公司人员，请勿入内")
