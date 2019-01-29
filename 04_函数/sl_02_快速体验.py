06e2c3da8d8af97991a0ddcb72be96aa63eec483
shengli <819829361@qq.com>
2019-01-29 22:28:18 +0800

    Add files via upload

---
+++ 04_函数/sl_02_快速体验.py
@@ -0,0 +1,3 @@
+import sl_01_九九乘法表
+
+sl_01_九九乘法表.multiple_table()
---
+++ 04_函数/sl_03_第一个函数.py
@@ -0,0 +1,8 @@
+# 注意：定义好函数之后，只表示这个函数封装了一段代码而已
+# 如果不主动调用函数函数是不会主动执行的
+def say_hello():
+    print("hello 1")
+    print("hello 2")
+    print("hello 3")
+
+say_hello()
