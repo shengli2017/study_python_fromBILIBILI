7c3f116e9fc64070d2c1531e5d40a85a58f5b7d6
shengli2017 <819829361@qq.com>
2019-03-03 22:44:13 +0800

    10_异常

---
+++ 10_异常/sl_03_捕获未知错误.py
@@ -0,0 +1,14 @@
+try:
+    # 提示用户输入一个整数
+    num = int(input("请输入一个整数："))
+
+    # 使用 8 除以用户输入的整数并且输出
+    result = 8 / num
+
+    print(result)
+
+except ValueError:
+    print("请输入正确的整数")
+
+except Exception as result:
+    print("未知错误 %s" % result)
---
+++ 10_异常/sl_05_异常的传递.py
@@ -0,0 +1,13 @@
+def demo1():
+    return int(input("输入整数："))
+
+def demo2():
+
+    return demo1()
+
+# 利用异常的传递性
+try:
+    print(demo2())
+
+except Exception as result:
+    print("未知错误 %s" % result)
