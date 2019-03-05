f8525e94f4cb9098aff15455241db7bb819d2078
shengli2017 <819829361@qq.com>
2019-03-05 22:13:32 +0800

    13_文件

---
+++ 13_文件/sl_02_读取文件后文件指针会改变.py
@@ -0,0 +1,17 @@
+# 1. 打开文件
+file = open("README", encoding="UTF-8")
+
+# 2. 读取文件内容
+text = file.read()
+print(text)
+print(len(text))
+
+print("=" * 50)
+
+text = file.read()
+print(text)
+print(len(text))
+
+
+# 3. 关闭文件
+file.close()
