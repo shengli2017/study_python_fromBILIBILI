549bcfd5a2754d3f5e887b4e24b71d03082be7cc
shengli2017 <819829361@qq.com>
2019-03-05 08:09:35 +0800

    11_模块

---
+++ 11_模块/sl_03_import导入模块.py
@@ -0,0 +1,11 @@
+import sl_01_测试模块1
+import sl_02_测试模块2
+
+sl_01_测试模块1.say_hello()
+sl_02_测试模块2.say_hello()
+
+dog = sl_01_测试模块1.Dog()
+print(dog)
+
+cat = sl_02_测试模块2.Cat()
+print(cat)
---
+++ 11_模块/sl_04_import同时指定别名.py
@@ -0,0 +1,12 @@
+import sl_01_测试模块1 as DogModule
+import sl_02_测试模块2 as CatModule
+
+DogModule.say_hello()
+CatModule.say_hello()
+
+dog = DogModule.Dog()
+print(dog)
+
+cat = CatModule.Cat()
+print(cat)
+
